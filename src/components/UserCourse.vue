fc82089e2af9279f12f6662b47669b33d14458ff
Pablo Gomez <pablog.acc@gmail.com>
2021-03-09 11:35:02 -0300

    separate UserCourse component

---
+++ src/components/UserCourse.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card mt-2">
+        <img :src="course.attributes.cover" alt="image" width="300" class="ml-auto mr-auto">
+        <div class="card-body">
+            <h2 class="card-title">{{ course.attributes.name }}</h2>
+        </div>
+    </div>
+    <div class="row">
+        <h2 class="card-title mt-3">
+            <strong>Contenido curso</strong>
+        </h2>
+    </div>
+    <!-- Mostrar listado de contenidos de un curso -->
+    <div class="card-body">
+        <div class="card-title" v-for="content in course.attributes.learning" :key="content.name">
+            <div class="card mt-2">
+                <div class="card-body d-flex justify-content-between">
+                    <h2>{{ content.name }}</h2>
+                    <h4 class="card-title mr-1">{{ content.status }}</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    course: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/UserCourseDetail.vue
+++ src/views/UserCourseDetail.vue
@@ -9,28 +9,9 @@
     </div>
 
     <div v-else class="row container mx-auto">
-        <div class="card mt-2">
-            <img :src="course.attributes.cover" alt="image" width="300" class="ml-auto mr-auto">
-            <div class="card-body">
-                <h2 class="card-title">{{ course.attributes.name }}</h2>
-            </div>
-        </div>
-        <div class="row">
-            <h2 class="card-title mt-3">
-                <strong>Contenido curso</strong>
-            </h2>
-        </div>
-        <!-- Mostrar listado de contenidos de un curso -->
-        <div class="card-body">
-            <div class="card-title" v-for="content in course.attributes.learning" :key="content.name">
-                <div class="card mt-2">
-                    <div class="card-body d-flex justify-content-between">
-                        <h2>{{ content.name }}</h2>
-                        <h4 class="card-title mr-1">{{ content.status }}</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+        <UserCourse :course=course />
+        
         <div class="row container">
             <router-link id="back" class="btn btn-secondary mt-2" to="/">Back</router-link>
         </div>
@@ -40,7 +21,12 @@
 </template>
 
 <script>
+import UserCourse from "@/components/UserCourse.vue"
+
 export default {
+    components: {
+        UserCourse,
+    },
     data(){
         return {
             loading: false,
